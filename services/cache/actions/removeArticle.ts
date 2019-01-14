9c562798da173e4829b1b04bdac025a0fc07e7fc
Bartłomiej Górnicki <brutusgsm@gmail.com>
2019-01-14 18:40:26 +0100

    Try to remove article from cache only when it exists

--- services/cache/actions/removeArticle.ts
+++ services/cache/actions/removeArticle.ts
@@ -4,12 +4,16 @@ const redis = new Redis({ host: "redis" });
 
 async function removeArticle(username: string, permlink: string) {
     try {
-        const cachedArticle = await redis.get(`article:${username}:${permlink}`);
-        const article = JSON.parse(cachedArticle);
-
         await redis.del(`article:${username}:${permlink}`);
         await redis.zrem(`created:${username}`, `article:${username}:${permlink}`);
-        await redis.zrem(`category:${username}:${article.category.slug}`, `article:${username}:${permlink}`);
+        
+        const cachedArticle = await redis.get(`article:${username}:${permlink}`);
+        const article = JSON.parse(cachedArticle);
+        
+        if(article) {
+            await redis.zrem(`category:${username}:${article.category.slug}`, `article:${username}:${permlink}`);
+        }
+        
     } catch (error) {
         console.log(error);
     }
