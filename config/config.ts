9918575732b802319b1c9992e904fe657158f341
Bartłomiej Górnicki <brutusgsm@gmail.com>
2019-03-07 20:17:50 +0100

    Services paths updated

--- config/config.ts
+++ config/config.ts
@@ -1,10 +1,16 @@
 export default {
     "services": {
-        "api-gateway": "api-gateway:3000",
-        "image-uploader": "image-uploader:3000",
-        "statistics": "statistics:3000",
+        "api_gateway": "api-gateway:3000",
+        "auth": "auth:3000",
+        "blockchain_tracker": "blockchain-tracker:3000",
+        "blogs_renderer": "blogs-renderer:3000",
+        "image_uploader": "image-uploader:3000",
+        "mailer": "mailer:3000",
+        "nginx_configurator": "nginx-configurator:3000",
+        "sitemap_builder": "sitemap-builder:3000",
         "ssl": "ssl:3000",
-        "nginx_configurator": "nginx-configurator:3000"
-        
+        "statistics": "statistics:3000",
+        "vault_connector": "vault-connector:3000",
+        "webpush_sender": "webpush-sender:3000"
     }
 }
