ad492703204894dd7c31537c4018ae057dcc43ec
Bartłomiej Górnicki <bartlomiej@gornicki.org>
2019-03-27 00:40:56 +0100

    Change scope order for Steemconnect connector

--- services/steemconnect/steemconnect.service.ts
+++ services/steemconnect/steemconnect.service.ts
@@ -8,7 +8,7 @@ import comment from './actions/comment'
 const dashboard = sc2.Initialize({
     app: process.env.STEEMCONNECT_ID,
     callbackURL: process.env.SC2_REDIRECT_DASHBOARD,
-    scope: ['offline', 'login', 'vote', 'comment', 'comment_options', 'claim_reward_balance','delete_comment']
+    scope: ['offline', 'login', 'vote', 'comment', 'delete_comment', 'comment_options', 'claim_reward_balance']
 });
 
 const blog = sc2.Initialize({
