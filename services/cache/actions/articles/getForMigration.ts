62a65a858207f07cf5dbcf01880f8fc31e47b5c9
Bartłomiej Górnicki <bgor91@gmail.com>
2019-08-28 22:24:55 +0200

    chore: migration

---
+++ services/cache/actions/articles/getForMigration.ts
@@ -0,0 +1,26 @@
+import engine from "../../store/engine";
+
+async function getForMigration() {
+    let posts:any = [];
+    
+    const keys = await engine.keys('article:');
+
+    console.log(keys);
+
+    for(const key in keys) {
+        const article = await engine.get(key);
+        if(article) {
+            const parsed = JSON.parse(article);
+            if(parsed && parsed.state != 404) {
+                posts.push({
+                    username: key.split(':')[2],
+                    permlink: parsed.permlink,
+                    category: parsed.category.slug
+                })
+            }
+        }
+    }
+    return posts;
+}
+
+export default getForMigration;
